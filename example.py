17d971ca1ecefe527bfd4db3f233f89bcfaa0ce8
indieleet <indieleet@gmail.com>
2024-03-06 02:10:57 -0500

    added some useful info about patterns

--- example.py
+++ example.py
@@ -7,31 +7,47 @@ def i1(freq, dur, vel, hz):
     return np.resize(np.linspace(-1, 1, int(freq)), int(dur))*vel*env
 def i2(freq, dur, vel, hz):
     return (np.random.rand(dur)*2 - 1)*np.append(np.linspace(0, 1, int(1/4*dur)), np.zeros(int(3/4*dur)))
+def i3(freq, dur, vel, hz):
+    env = np.resize(np.linspace(1, 0, int(dur*2/3)), int(dur))
+    return np.resize(np.sin(np.sin(np.linspace(-1,1,int(freq))*2*np.pi)*2*np.pi), int(dur))*vel*env
 t = genevative.Tracker()
 t.add_pattern(
         [[440, 1, 1],
          [i1, 1, 1, 1],
          [i1, 1, 1, 1],
          [i1, 1/3, 2, 1],
-         [i1, 1, 1, 1],
+         [i1, 1, 1/2, 1],
          [i1, 1, 1, 1],
          [i1, 3/1, 1, 1],
          [i1, 1, 1, 1],
          [i1, 2/3, 1/2, 1],
-         [i1, 1, 1, 1],
+         [i1, 1, 1/3, 1],
          [i1, 1, 1, 1],
          [i1, 1, 1/2, 1],
-         [i1, 1, 1, 1],
+         [i1, 1, 1/4, 1],
          [i1, 1, 1, 1],
          [i1, 1, 1, 1],
          [i1, 2, 1/2, 0.75]])
 t.add_pattern(
         [[1, 1, 1],
-         [i2,1, 1],
-         [i2,1/2, 1],
-         [i2,2, 1],
-         [i2,1, 1],
-         [i2,1, 1],
-         [i2,1/3, 1],
-         [i2,1, 1]])
+         [i2, 1, 1],
+         [i2, 1/2, 1],
+         [i2, 2, 1],
+         [i2, 1, 1],
+         [i2, 1, 1],
+         [i2, 3, 1],
+         [i2, 7/3, 1],
+         [i2, 1, 1],
+         [i2, 1/3, 1],
+         [i2, 1, 1]])
+t.add_pattern(
+    [[440, 1, 1],
+     [i3, 3/2, 1/3, 0.5],
+     [i3, 1, 7/3, 1],
+     [i3, 1, 1/3, 1],
+     [i3, 1/2, 5/3, 2],
+     [i3, 1, 4/3, 2],
+     [i3, 3/2, 1/3, 1],
+     [i3, 1/2, 3, 1],
+     [i3, 3/2, 1/3, 1]])
 t.render()
--- genevative.py
+++ genevative.py
@@ -18,7 +18,11 @@ class Tracker():
         vel = 1
         pre_rendered = []
         rendered = []
-        for pattern in self.__raw_pattern:
+        for pat_num, pattern in enumerate(self.__raw_pattern):
+            total_freq = 1
+            total_dur = 1
+            total_vel = 1
+            total_len = 1
             curr_pattern = np.array([], dtype="float64")
             for n, line in enumerate(pattern):
                 if n == 0:
@@ -30,17 +34,21 @@ class Tracker():
                 if line_len > 0:
                     instr = line[0]
                 else:
-                    instr = lambda x,y,z:0
+                    instr = lambda x,y,w,z:0
                 if line_len > 1:
                     freq *= line[1]
+                    total_freq *= line[1]
                 if line_len > 2:
                     dur *= line[2]
+                    total_dur *= line[2]
                 if line_len > 3:
                     vel *= line[3]
+                    total_vel *= line[3]
                 #TODO: make args to fn optional
                 curr_pattern = np.append(curr_pattern, instr(self.sample_rate/freq, dur*self.sample_rate, vel, self.sample_rate))
             pre_rendered.append(curr_pattern.copy())
+            total_len = len(curr_pattern)/self.sample_rate
+            print(f"n:{pat_num:.2f} f:{total_freq:.2f} d:{total_dur:.2f} v:{total_vel:.2f} l:{total_len:.2f}")
             max_len = max([len(i) for i in pre_rendered])
             rendered = np.sum([np.append(i, np.zeros(max_len - len(i))) for i in pre_rendered], axis=0)
-            print(rendered)
             wav_render("out.wav", self.sample_rate, rendered)
